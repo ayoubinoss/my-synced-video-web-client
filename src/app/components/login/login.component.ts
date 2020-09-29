c475fc71592da8d7bcf60d45d26dfa87b78946ab
ayoubinoss <ayoubaitbahaddou@gmail.com>
2020-09-29 21:51:26 +0100

    "MSV-1 Retrieve values from the html form"

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   
-  createRoom() {}
+  public loginForm: FormGroup;
+  public userName: FormControl;
+  public roomName: FormControl;
 
-  constructor() {
+  constructor(
+    private formBuilder:FormBuilder,
+    private router: Router
+    ) {
+      this.loginForm = formBuilder.group({
+        userName: this.userName = new FormControl('', Validators.required),
+        roomName: this.roomName = new FormControl('', Validators.required)
+      })
   }
 
   ngOnInit(): void {
+  }
+
+  createRoom() {
+    let userName = this.loginForm.get('userName').value;
+    let roomName = this.loginForm.get('roomName').value;
+    if(this.loginForm.valid) {
+      //TO-DO create service to send data to server
+    }
   }
 
 }
